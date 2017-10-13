bb28df11c8baef240359ba7461ec9f7dbf893d6b
xiaohao <ganxiaohao520@163.com>
2017-10-13 18:01:59 +0800

    11

---
+++ app/src/main/java/com/test/activity/GridViewAdapter.java
@@ -0,0 +1,69 @@
+package com.test;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.LinearInterpolator;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.test.ninegrid.Image;
+import com.test.ninegrid.L;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * Created by xiaohao on 17-10-9.
+ */
+
+public class GridViewAdapter extends BaseAdapter {
+    private Context context;
+    private ArrayList<String> images;
+
+    public GridViewAdapter(Context context, ArrayList<String> images) {
+        System.out.println("适配器");
+        this.context = context;
+        this.images = images;
+    }
+
+    @Override
+    public int getCount() {
+        return images.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return i;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        System.out.println("getview");
+        LinearLayout linearLayout = (LinearLayout) LayoutInflater.from(context).inflate(R.layout.gridview_item, null);
+        ImageView imageView = linearLayout.findViewById(R.id.gridviewImage);
+        File file = new File(images.get(i));
+        if (file.exists()) {
+            System.out.println("文件存在");
+            //    imageView.setImageResource(R.drawable.selector_grid_camera_bg);
+          imageView.setImageBitmap(BitmapFactory.decodeFile(images.get(i)));
+
+
+            return imageView;
+        } else {
+            System.out.println(images.get(i)+"文件不存在");
+        }
+
+
+        return null;
+    }
+}
---
+++ app/src/main/java/com/test/activity/ImagePickers.java
@@ -0,0 +1,30 @@
+package com.test;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.widget.ImageView;
+
+import com.lzy.imagepicker.loader.ImageLoader;
+import com.squareup.picasso.MemoryPolicy;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+
+/**
+ * Created by xiaohao on 17-10-8.
+ */
+
+public class ImagePickers implements ImageLoader {
+    @Override
+    public void displayImage(Activity activity, String path, ImageView imageView, int width, int height) {
+        Picasso.with(activity).load(Uri.fromFile(new File(path)))
+                .placeholder(R.mipmap.default_image).error(R.mipmap.default_image)
+                .resize(width,height).centerInside().memoryPolicy(MemoryPolicy.NO_CACHE,MemoryPolicy.NO_STORE)
+                .into(imageView);
+    }
+
+    @Override
+    public void clearMemoryCache() {
+
+    }
+}
